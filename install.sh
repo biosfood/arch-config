804122d2cd8c7320ed8851eef97e91cf5fceb4d7
biosfood <leisenhau@gmail.com>
2023-05-28 19:40:02 +0200

    add plasmaConfiguration to install script

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ read -p "Do you want to install essential packages? (y / N): "
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
 	echo "Installing bare essenital programs using paru..."
-	sudo pacman -S tree --noconfirm
-	paru -S pulse-browser-bin konsole tbsm zettlr --noconfirm
+	sudo pacman -S tree vi gnupg --noconfirm
+	paru -S pulse-browser-bin konsole tbsm zettlr konsave --noconfirm
 fi
 
 read -p "Do you want to insert default configuration files? (y / N): "
@@ -15,4 +15,8 @@ if [[ $REPLY =~ ^[Yy]$ ]]
 then
 	echo "Inserting configuration for Konsole . . ."
 	cp .local/share/konsole/Profile\ 1.profile ~/.local/share/konsole/Profile\ 1.profile
+
+	echo "Importing plasma desktop configuration"
+	konsave -i plasmaConfig.knsv
+	konsave -a plasmaConfig
 fi
