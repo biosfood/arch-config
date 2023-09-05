d64bfd17ba7230b8836114eea2c4b342aef6b6ea
biosfood <leisenhau@gmail.com>
2023-09-05 21:21:33 +0200

    fix postgresql: bad home directory

--- postgresql.sh
+++ postgresql.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-initdb --locale en_US.UTF-8 -D '~/postgres/data'
+initdb --locale en_US.UTF-8 -D ~/postgres/data
 sudo mkdir /run/postgresql
 sudo chown lukas /run/postgresql/
 pg_ctl -D ~/postgres/data start
